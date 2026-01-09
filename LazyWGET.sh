b62e4f3d764ea5634da7194e5ea9791688664605
Carlos <carlosavbos@gmail.com>
2026-01-09 15:17:20 +0000

    Verific integritatea URL-ului cu --spider, preiau numele fileului si adaug fisiere de stare (.lwget) pentru evaluarea lazy: pending si downloaded

--- LazyWGET.sh
+++ LazyWGET.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # in prima faza, luam link-ul de la tastatura ca argument (primul)
-# argumentul va fii salvat in variabila UNVERIFIED_URL
+# argumentul va fii salvat in variabila RECEIVED_URL
 
-unverified_url=$1
+received_url=$1
 
 if [[ -z "$1" ]]; then
     echo "Empty argument, NOT proceding..."
-    exit 1
+    exit 1 #failsafe, in orice caz
 fi
 
 # file urile de tip HTML vor fii descarcare in directorul DOWNLOADS
@@ -33,5 +33,29 @@ fi
 # informam user-ul unde vor fii salvate file-urile
 echo "Files will be downloaded in $dir_download"
 
-# echo "$unverified_url"
-# echo "$dir_download"
+# verificam cu --spider daca URL-ul poate fi deschis (conexiune okay, server okay etc.)
+if wget --spider "$received_url"; then
+    echo "URL opened with success, proceeding..."
+else
+    echo "URL could not be opened, NOT proceeding..."
+    exit 1 #failsafe iarasi
+fi
+
+#preluam numele file-ului, ${received_url%%[\?#]*} taie tot ce urmeaza dupa ? sau #
+# si basename da ultimul segment 
+name="$(basename -- "${received_url%%[\?#]*}")"
+
+#apare totusi problema cand URL-ul se termina cu /, asa ca il vom trata ca index daca name devine gol
+if [[  -z "$name" ]]; then
+    name="index"
+elif [[ "$name" != *.html ]]; then
+    name+=".html"
+fi
+
+wget "$received_url" -O "$dir_download/$name"
+# in cazul in care unele URL-uri mai primare contin .html la final de URL, il pastram,
+# iar daca nu au il adaugam, astfel sigur orice ajunge la wget-ul de download are extensia HTML
+
+mkdir "$dir_download/.lwget/" # fisierul de istoric
+touch "$dir_download/.lwget/pending.txt" # lista de promisiuni evaluate
+touch "$dir_download/.lwget/downloaded.txt" # lista de file uri descarcate anterior
